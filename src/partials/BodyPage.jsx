8d2347ded63ba807b567a49fdad11ed39effd4ae
Emi <emi.lewis71@gmail.com>
2024-11-21 20:37:54 +0000

    Finished Site, hopefully.

--- src/partials/BodyPage.jsx
+++ src/partials/BodyPage.jsx
@@ -9,11 +9,15 @@ const BodyPage = ({ children, title = "Trans Advocacy and Complaint Collective",
       <title>{title}</title>
       <meta charSet="UTF-8"/>
       <meta name="description" content={description}/>
-      <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700;900&#038;display=swap' rel='stylesheet'/>
+      <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700;900&#038;display=swap'
+            rel='stylesheet'/>
       <link href="/styles/bootstrap.min.css" rel="stylesheet"/>
       <link href="/styles/style.css" rel="stylesheet"/>
       <script src="/js/bootstrap/bootstrap.bundle.min.js"></script>
-      <link rel="icon" href="/media/trans.png"/>
+      <link rel="icon" href="/media/tacc_icon.png" sizes="32x32"/>
+      <link rel="icon" href="/media/tacc_icon.png" sizes="192x192"/>
+      <link rel="apple-touch-icon" href="/media/tacc_icon.png"/>
+      <meta name="msapplication-TileImage" content="/media/tacc_icon.png"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <meta name="theme-color" content="#121212"/>
     </head>
--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import {FaBluesky, FaDiscord, FaFacebook, FaThreads} from "react-icons/fa6";
 
 const Header = () => {
   return (
@@ -9,15 +10,15 @@ const Header = () => {
           <img className="logo" src="/media/tacc.png" alt="Trans Advocacy and Complaint Collective Logo"/>
         </Navbar.Brand>
 
-      {/* Toggle button for mobile view with baked-in Bootstrap JS */}
+        {/* Toggle button for mobile view with baked-in Bootstrap JS */}
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbar-menu"
-          aria-controls="navbar-menu"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbar-menu"
+            aria-controls="navbar-menu"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
 
         >
           <span className="navbar-toggler-icon"></span>
@@ -41,6 +42,20 @@ const Header = () => {
             <Nav.Link href="/get-involved.html">Get Involved</Nav.Link>
 
           </Nav>
+        </div>
+        <div className="social-links d-flex justify-content-center">
+          <a href="https://discord.gg/KMxjhabAxN" aria-label="Discord" className="social">
+            <FaDiscord size={30} style={{color: "#1976d", paddingBottom: '4px'}}/>
+          </a>
+          <a href="https://bsky.app/profile/tacc-uk.bsky.social" className="mx-2" aria-label="Bluesky">
+            <FaBluesky size={24} style={{color: "#1976d"}}/>
+          </a>
+          <a href="https://www.threads.net/@tacc40566" aria-label="Threads">
+            <FaThreads size={24} style={{color: "#1976d"}}/>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61567774272432" className="mx-2" aria-label="Facebook">
+            <FaFacebook size={24} style={{color: "#1976d"}}/>
+          </a>
         </div>
       </Container>
     </Navbar>
