776bb93c73ced1ce24cbd634de391c3a1b8e838d
Emi <emi.lewis71@gmail.com>
2024-11-29 15:27:28 +0000

    Accidently uploaded an old version of css.

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -32,23 +32,6 @@ const Footer = () => {
               </a>
             </div>
           </Col>
-          {/*<Col md={4} className="text-center">*/}
-          {/*  <p className="mb-2 mb-md-0 footer">Follow us:</p>*/}
-          {/*<div className="social-links d-flex justify-content-center">*/}
-          {/*  <a href="https://discord.gg/KMxjhabAxN" aria-label="Discord" className="social">*/}
-          {/*    <FaDiscord size={30} style={{color: "black", paddingBottom: '4px'}} />*/}
-          {/*  </a>*/}
-          {/*  <a href="https://bsky.app/profile/tacc-uk.bsky.social"  className="mx-2">*/}
-          {/*    <FaBluesky size={24} style={{color: "black"}}/>*/}
-          {/*  </a>*/}
-          {/*  <a href="https://www.threads.net/@tacc40566" aria-label="Threads">*/}
-          {/*    <FaThreads size={24} style={{color: "black"}}/>*/}
-          {/*  </a>*/}
-          {/*  <a href="https://www.facebook.com/profile.php?id=61567774272432" className="mx-2" aria-label="Facebook">*/}
-          {/*    <FaFacebook size={24} style={{color: "black"}}/>*/}
-          {/*  </a>*/}
-          {/*</div>*/}
-          {/*</Col>*/}
         </Row>
       </Container>
     </footer>
